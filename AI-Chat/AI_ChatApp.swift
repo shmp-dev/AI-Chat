191c8b9a7570dac433cd35274cf4f0a4daffbaae
shmp-dev <shmp.dev@gmail.com>
2023-04-09 16:18:21 +0900

    Initial Commit

---
+++ AI-Chat/AI_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_ChatApp.swift
+//  AI-Chat
+//
+//  Created by shumpei shimizu on 2023/04/09.
+//
+
+import SwiftUI
+
+@main
+struct AI_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
